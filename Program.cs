8638f384ae52496699895c93382620e75d1463b4
Samuray279 <pyan8106@gmail.com>
2022-11-11 21:31:52 -0300

    mais um pronto

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("-MúltiplO-\n");
+Console.Write("Digite um número...: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Avaliar se é múltiplo de:");
+int N2 = Convert.ToInt32(Console.ReadLine());
+if (N1 % N2 == 0)
+    Console.WriteLine($"\n{N1} é múltiplo de {N2}");
+else
+  Console.WriteLine($"\n{N1} não é múltiplo de {N2}");
